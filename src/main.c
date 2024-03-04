b35255df1c2db8e5701376f12f1e56dec916942a
jberkema <jberkema@student.42heilbronn.de>
2024-03-04 11:19:08 +0100

    test

--- src/main.c
+++ src/main.c
@@ -1,37 +1,37 @@
 #include "cube.h"
 
-int	main()
-{
-	test_with_mocks();
-	t_game	*game;
-
-	game = init_game();
-	if (!game)
-	{
-		printf("Error: failed to initialize game\n");
-		return (1);
-	}
-	mlx_image_to_window(game->graphics->mlx, game->graphics->image, 0, 0);
-	mlx_loop(game->graphics->mlx);
-	return (0);
-}
-
-// int	main(int ac, char **argv)
+// int	main()
 // {
-// 	t_map	*m;
-// 	int		fd;
+// 	test_with_mocks();
+// 	t_game	*game;
 
-// 	if (ac == 2)
+// 	game = init_game();
+// 	if (!game)
 // 	{
-// 		m = ft_calloc(1, sizeof(t_map));
-// 		fd = open(argv[1], O_RDONLY);
-// 		if (fd < 0)
-// 			return (ft_prerr(INV_FD, NULL), 1);
-// 		if (parse(m, fd, argv[1]) == NULL)
-// 			return (ft_prerr(PARSING_FAILED, NULL), 1);
+// 		printf("Error: failed to initialize game\n");
+// 		return (1);
 // 	}
-// 	else
-// 		return (ft_prerr(WRONG_ARG, NULL), 1);
-// 	free_map_struct(m);
+// 	mlx_image_to_window(game->graphics->mlx, game->graphics->image, 0, 0);
+// 	mlx_loop(game->graphics->mlx);
 // 	return (0);
 // }
+
+int	main(int ac, char **argv)
+{
+	t_map	*m;
+	int		fd;
+
+	if (ac == 2)
+	{
+		m = ft_calloc(1, sizeof(t_map));
+		fd = open(argv[1], O_RDONLY);
+		if (fd < 0)
+			return (ft_prerr(INV_FD, NULL), 1);
+		if (parse(m, fd, argv[1]) == NULL)
+			return (ft_prerr(PARSING_FAILED, NULL), 1);
+	}
+	else
+		return (ft_prerr(WRONG_ARG, NULL), 1);
+	free_map_struct(m);
+	return (0);
+}
