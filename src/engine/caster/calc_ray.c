853ede715b06e534565614069884ae715c7e0141
jberkema <jberkema@student.42heilbronn.de>
2024-03-24 22:42:06 +0100

    removed printf and comments

--- src/engine/caster/calc_ray.c
+++ src/engine/caster/calc_ray.c
@@ -62,17 +62,13 @@ void	set_dda(t_position *hitpoint, t_game *game, t_vector *d, int version)
 {
 	t_vector	*step;
 
-	printf("set_dda\n");
 	step = &game->ray.side_dist_x;
 	if (version == 0)
 		step = &game->ray.side_dist_y;
 	set_wall_direction(d, version, game);
 	set_hitpoint(hitpoint, game, version);
 	if (hitpoint->x > 0 && hitpoint->y > 0)
-	{
 		add_one_step(step, d);
-		printf("add step\n");
-	}
 }
 
 void	dda(t_position *hitpoint, t_game *game, t_vector *dx, t_vector *dy)
@@ -91,45 +87,10 @@ void	dda(t_position *hitpoint, t_game *game, t_vector *dx, t_vector *dy)
 		}
 		if (flag == 1 || vector_length(&game->ray.side_dist_x) < \
 			vector_length(&game->ray.side_dist_y))
-		{
 			set_dda(hitpoint, game, dx, 1);
-			printf("DX");
-			// set_wall_direction(dx, 1, game);
-			// set_hitpoint(hitpoint, game, 1);
-			// if (hitpoint->x > 0 && hitpoint->y > 0)
-			// 	add_one_step(&game->ray.side_dist_x, dx);
-		}
 		else
-		{
 			set_dda(hitpoint, game, dy, 0);
-			printf("DY");
-			// set_wall_direction(dy, 0, game);
-			// set_hitpoint(hitpoint, game, 0);
-			// if (hitpoint->x > 0 && hitpoint->y > 0)
-			// 	add_one_step(&game->ray.side_dist_y, dy);
-		}
 		if (check_hit(game, (*hitpoint)) == 1)
 			return ;
 	}
-}
-
-void	calculate_hitpoint(t_game *game)
-{
-	t_vector	factor;
-	t_position	hitpoint;
-	t_vector	dx;
-	t_vector	dy;
-
-	factor = set_first_block_border(game);
-	dx = game->ray.step_for_plus_x;
-	dy = game->ray.step_for_plus_y;
-	if (check_first_wall(game, factor, &hitpoint) == 1)
-	{
-		game->ray.hitpoint.x = hitpoint.x;
-		game->ray.hitpoint.y = hitpoint.y;
-		return ;
-	}
-	dda(&hitpoint, game, &dx, &dy);
-	game->ray.hitpoint.x = hitpoint.x;
-	game->ray.hitpoint.y = hitpoint.y;
 }
