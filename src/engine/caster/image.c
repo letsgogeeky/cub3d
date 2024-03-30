1cdde2e44af07afcd35aae45e4270f7c8121aed1
jberkema <jberkema@student.42heilbronn.de>
2024-03-30 10:23:23 +0100

    norminette image.c

--- src/engine/caster/image.c
+++ src/engine/caster/image.c
@@ -1,31 +1,45 @@
 #include "cube.h"
 
-void	resze_tex(t_texture *normal, t_game *game, t_column *column, int x)
+double	set_col(t_game *game, t_texture *normal)
 {
-	double	col;
-	int		n;
-	double	j;
-	double	step;
-	double	y;
+	int	col;
 
 	if (game->ray.wall_texture == NORTH || game->ray.wall_texture == SOUTH)
 		col = normal->width * fmod(game->ray.hitpoint.x, 1);
 	else
 		col = normal->width * fmod(game->ray.hitpoint.y, 1);
+	return (col);
+}
+
+void	set_short(t_texture *normal, t_game *game, double *y, double *step)
+{
+	(*y) = (double)normal->height / 2.0 - 0.5 * (double)normal->height * \
+		((double)HEIGHT / game->ray.wall_height);
+	(*step) = (double)normal->height / (game->ray.wall_height \
+		/ (double)HEIGHT) / (double)HEIGHT;
+}
+
+void	resze_tex(t_texture *normal, t_game *game, t_column *column, int x)
+{
+	double	col;
+	double	j;
+	double	step;
+	double	y;
+	int		n;
+
+	col = set_col(game, normal);
 	y = 0;
-	if (game->ray.wall_height >= (double)HEIGHT)
-		y = (double)normal->height / 2.0 - 0.5 * (double)normal->height * ((double)HEIGHT / game->ray.wall_height);
 	step = 1.0 * normal->height / column->wall_height;
 	if (game->ray.wall_height >= (double)HEIGHT)
-		step = (double)normal->height / (game->ray.wall_height / (double)HEIGHT) / (double)HEIGHT;
-	n = column->end_ceiling;
+		set_short(normal, game, &y, &step);
 	j = ((int)y * normal->width) + col;
+	n = column->end_ceiling;
 	while (n < column->start_floor && n < HEIGHT)
 	{
 		if (j > normal->width * normal->height)
 			j = normal->width * normal->height - 1;
 		mlx_put_pixel(game->image, x, n, normal->pixels[(int)j]);
-		y+= step;
+		y += step;
 		j = ((int)y * normal->width) + col;
 		n++;
 	}
